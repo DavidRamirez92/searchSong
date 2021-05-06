e22cb6dc64334f174a8ed06db2d8cfa4306195f3
David <davidramirez9210@gmail.com>
2021-05-06 18:17:49 -0300

    Inicial Setup to the proyect

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,64 @@
+import React,{useState} from 'react';
+
+const Formulario = () => {
+    const [busqueda,guardarBusqueda] = useState({
+        artista:'',
+        cancion:''
+    })
+    //funcion a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+    return ( 
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form 
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                        >
+                            <fieldset>
+                                <legend className="text-center">Buscador Letas Canciones</legend>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <label>Artista</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="artista"
+                                                placeholder="Nombre Artista"
+                                                onChange={actualizarState}
+                                            />
+                                        </div>
+                                       
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <label>Cancion</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="cancion"
+                                                placeholder="Nombre Cancion"
+                                                onChange={actualizarState}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <button 
+                                    type="submit"
+                                    className="btn btn-primary float-right"
+                                    >Buscar</button>
+                            </fieldset>
+
+                        </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Formulario;
