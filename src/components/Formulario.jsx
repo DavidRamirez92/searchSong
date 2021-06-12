2b2b985f5271d950410ee7de10f0cf57231350e0
David <davidramirez9210@gmail.com>
2021-06-12 19:48:02 -0300

    finish project

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -39,7 +39,7 @@ const Formulario = ({guardarBusquedaLetra}) => {
                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
                         >
                             <fieldset>
-                                <legend className="text-center">Buscador Letas Canciones</legend>
+                                <legend className="text-center">Buscador Letras Canciones</legend>
                                 <div className="row">
                                     <div className="col-md-6">
                                         <div className="form-group">
